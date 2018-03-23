36e34895f1e22cffb68d4f871a55b6efac82a9f5
ytj <1371467458@qq.com>
2018-03-23 19:35:00 +0800

    后台自动更新天气功能和更换Logo

--- app/src/main/java/com/yyytj/myweather/WeatherActivity.java
+++ app/src/main/java/com/yyytj/myweather/WeatherActivity.java
@@ -1,5 +1,6 @@
 package com.yyytj.myweather;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Build;
@@ -21,6 +22,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.yyytj.myweather.gson.Forecast;
 import com.yyytj.myweather.gson.Weather;
+import com.yyytj.myweather.service.AutoUpdateService;
 import com.yyytj.myweather.utils.HttpUtil;
 import com.yyytj.myweather.utils.Utility;
 
@@ -250,5 +252,7 @@ public class WeatherActivity extends AppCompatActivity {
         sportText.setText(sport);
         // 设置布局可见
         weatherLayout.setVisibility(View.VISIBLE);
+        Intent intent = new Intent(this, AutoUpdateService.class);
+        startService(intent);
     }
 }
